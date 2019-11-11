55ba6e7bc87ff9ba37707e7b7eabbcac5289fa89
Ernie Hsiung <ernie@whereby.us>
2019-11-11 17:48:04 -0500

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import GridList from '@material-ui/core/GridList';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+
 // import feedData from './feedData';
 import axios from 'axios';
 import MTGCard from './components/MTGCard.js'
@@ -15,19 +18,34 @@ import './App.css';
 
 function App() {
   const [feedData, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loadMore, setLoadMore] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'https://api.magicthegathering.io/v1/cards?type=Creature&pageSize=20'
+        `https://api.magicthegathering.io/v1/cards?type=Creature&pageSize=20&page=${page}`
       );
-      setData(result.data.cards);
+      setData(prevData => [...prevData, ...result.data.cards]);
     };
     fetchData();
+    setLoadMore(false);
+  }, [page]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+      console.log('Fetch more list items!');
+      setLoadMore(true);
+      setPage(p => p + 1)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
     <div className="App">
+      {loadMore && <CircularProgress />}
       <Container style={{
         display: 'flex',
         flexWrap: 'wrap',
