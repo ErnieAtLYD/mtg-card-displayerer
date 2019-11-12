7cb3e88025cd04c5f0c74defb743a5b71945738a
Ernie Hsiung <ernie@whereby.us>
2019-11-12 11:55:27 -0500

    fix: handle cancellation token

--- src/components/useInfiniteScroll.js
+++ src/components/useInfiniteScroll.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+let cancel;
+
 const useInfiniteScroll = () => {
   const [feedData, setData] = useState([]);
   const [page, setPage] = useState(1);
@@ -25,9 +27,16 @@ const useInfiniteScroll = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      if(cancel){
+        // Cancel the previous request before making a new request
+        cancel.cancel();
+      }
+      cancel = axios.CancelToken.source();
+
       try {
         const result = await axios(
-          `https://api.magicthegathering.io/v1/cards?type=Creature&pageSize=20&page=${page}`
+          `https://api.magicthegathering.io/v1/cards?type=Creature&pageSize=20&page=${page}`,
+          {cancelToken: cancel.token}
         );
         if (result.status === 200) {
           setData(prevData => [...prevData, ...result.data.cards]);
